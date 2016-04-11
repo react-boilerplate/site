eb119257663c7742e0362cc5173378d2823a00ec
webdesserts <michael@webdesserts.com>
2016-04-11 08:10:50 -0400

    basic styles & Getting Started

---
+++ app/components/GettingStarted/index.test.js
@@ -0,0 +1,9 @@
+import GettingStarted from './index';
+
+import expect from 'expect';
+import { shallow } from 'enzyme';
+import React from 'react';
+
+describe('<GettingStarted />', () => {
+
+});
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -8,30 +8,10 @@
 
 import React from 'react';
 
-import Img from 'Img';
-import Footer from 'Footer';
-import Banner from './banner-metal.jpg';
-import A from 'A';
-
 import styles from './styles.css';
 
 function App(props) {
-  return (
-    <div className={ styles.wrapper }>
-      <A
-        className={ styles.logoWrapper }
-        href="https://twitter.com/mxstbr"
-      >
-        <Img
-          className={ styles.logo }
-          src={ Banner }
-          alt="react-boilerplate - Logo"
-        />
-      </A>
-      { props.children }
-      <Footer />
-    </div>
-  );
+  return props.children;
 }
 
 export default App;
